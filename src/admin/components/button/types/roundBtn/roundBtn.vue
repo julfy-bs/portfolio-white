ffce47bdc8c55159aa6431ca9629b4fb05e58669
Bogdan Sutuzhko <bogdan.sutuzhko.19@gmail.com>
2021-03-24 09:51:10 +0300

    Свёрстан header, главная страница и заглушка

--- src/admin/components/button/types/roundBtn/roundBtn.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <button v-on="$listeners" type="submit" data-text="+" class="round-button-component"></button>
-</template>
-<style lang="postcss" scoped src="./roundBtn.pcss"></style>
